be6cc1379b4f801b5068f50c6f29dfce300e0225
Kyle-Neale <kyle.a.neale@gmail.com>
2018-12-19 07:20:17 -0500

    Added alternate iterative solution for problem.

--- Javascript/n_pyramids.js
+++ Javascript/n_pyramids.js
@@ -35,3 +35,25 @@ function nPyramid(n) {
     console.log(stair);
   }
 }
+
+// Alternate Solution
+function nPyramid(n) {
+  let midpoint = Math.floor((2 * n - 1)/ 2);
+  for (var row = 0; row < n; row++) {
+    let level = '';
+    for (var column = 0; column < 2 * n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
+    console.log(level);
+  }
+}
+
+
+//Recursive Solution
+function nPyramid(n) {
+
+}
