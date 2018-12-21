a8f8534644d4dc8049a2a8e158658b80f4945c15
Kyle-Neale <kyle.a.neale@gmail.com>
2018-12-21 16:29:14 -0500

    Recursive solution added for Fibonacci sequence.

--- Javascript/fibonacci.js
+++ Javascript/fibonacci.js
@@ -22,9 +22,8 @@ function fibonacci(n) {
 		return [];
 	} else if (n === 1) {
 		return [1];
-	} else if (n === 2) {
-		return [1, 1];
 	}
+
 	let fibArr = [1, 1];
 	while (fibArr.length < n) {
 		let nextNum = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
@@ -34,7 +33,6 @@ function fibonacci(n) {
 }
 
 // Recursive Solution:
-
 function fibonacci(n) {
 	if (n === 0) {
 		return [];
