d55e3e18be62b2aa18bce8bc1ac10f37ed1aab45
Kyle-Neale <kyle.a.neale@gmail.com>
2019-01-06 16:21:47 -0500

    Updated and added methods to LinkedList class

--- Javascript/datastructure_linked_list.js
+++ Javascript/datastructure_linked_list.js
@@ -3,7 +3,6 @@
   Implement classes Node and Linked List
 
 *******************************************************************************/
-
 class Node {
   constructor(data, next = null) {
     this.data = data;
@@ -27,7 +26,6 @@ class LinkedList {
       counter++;
       node = node.next;
     }
-
     return counter;
   }
 
@@ -36,11 +34,11 @@ class LinkedList {
   }
 
   getLast() {
-    let node = this.head;
-    if (!node) {
+    if (!this.head) {
       return null;
     }
 
+    let node = this.head;
     while (node.next) {
       node = node.next
     }
@@ -50,5 +48,28 @@ class LinkedList {
   clear() {
     this.head = null;
     return this.head;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    let node = this.head
+    while (node.next.next) {
+      node = node.next
+    }
+    node.next = null;
   }
 }
