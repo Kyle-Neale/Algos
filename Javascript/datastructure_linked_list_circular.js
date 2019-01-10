b4fbe07c959428d049b3cb76fd9e58dc208fa74a
Kyle-Neale <kyle.a.neale@gmail.com>
2019-01-10 12:50:44 -0500

    Added solution to circular problem for Linked List.

---
+++ Javascript/datastructure_linked_list_circular.js
@@ -0,0 +1,29 @@
+/*******************************************************************************
+Given a LinkedList find, return true if the list is circular, false if it is not.
+
+Example:
+  const l = new LinkedList();
+  const a = new Node('a');
+  const b = new Node('b');
+  const c = new Node('c');
+  l.head = a;
+  a.next = b;
+  b.next = c;
+  c.next = a;
+  circular(l) // => true;
+
+*******************************************************************************/
+
+const circular = (list) => {
+  let slow = list.getFirst();
+  let fast = list.getFirst();
+
+  while (fast.next.next && fast.next) {
+    if (slow === fast) {
+      return true;
+    }
+    slow = slow.next;
+    fast = fast.next.next
+  }
+  return false;
+}
