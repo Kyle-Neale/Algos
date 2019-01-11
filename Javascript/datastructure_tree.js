02d909125f19ecfde2a0835e87d30927eb533bf6
Kyle-Neale <kyle.a.neale@gmail.com>
2019-01-11 09:25:40 -0500

    Implemented add and remove methods for Node class.

---
+++ Javascript/datastructure_tree.js
@@ -0,0 +1,29 @@
+/********************************************************************************
+Create a Node class with a constructor that should accept an argument that gets
+assigned to the data property and initialize an empty array for storing children.
+The class should additionally have the methods 'add' and 'remove'.
+
+Create a Tree class with a constructor that has a 'root' property set to null.
+
+********************************************************************************/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter(element => element.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+}
