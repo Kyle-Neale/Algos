a9f45910deb6fa756696cbf8160ccd2bc827b183
Kyle-Neale <kyle.a.neale@gmail.com>
2019-01-11 19:41:15 -0500

    Implemented a recursive contains method for BST.

--- Javascript/datastructure_binary_seach_tree.js
+++ Javascript/datastructure_binary_seach_tree.js
@@ -1,9 +1,13 @@
 /********************************************************************************
 Create a Node class with a constructor that should accept an argument that gets
 assigned to the data property and initialize values 'left' and 'right'.
-Implement the 'insert' method for the Node class with should accept an argument
+
+Implement the 'insert' method for the Node class that should accept an argument
 'data', then create and insert a new node at the appropriate location in the
 tree.
+
+Implement a 'contains' method for the Node class that should accept a 'data' argument and return the Node in the tree with the same value. If the value isn't in the true,
+return null.
 
 ********************************************************************************/
 
@@ -26,6 +30,21 @@ class Node {
 
     } else if (this.data < data) {
       this.right = new Node(data);
+    }
+  }
+
+  contains(data) {
+    if (this.data === data) {
+      return data;
+    }
+    if (this.data < data && this.right) {
+      return this.right.contains(data);
+
+    } else if (this.data > data && this.left) {
+      return this.left.contains(data);
+
+    } else {
+      return null;
 
     }
   }
